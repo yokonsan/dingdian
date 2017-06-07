6926e62d54c9c11027c95d146b37b8fcd5e98710
YuKun <15152347277@163.com>
2017-06-07 19:58:52 +0800

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app
-# from app.models import User, Role, Permission, Post, Follow, Comment
 from flask_script import Manager, Shell
-# from flask_migrate import Migrate, MigrateCommand
 
 # gevent
 from gevent import monkey
@@ -14,26 +12,8 @@ monkey.patch_all()
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
-# migrate = Migrate(app, db)
-#
-#
-# def make_shell_context():
-#     return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
-#                 Permission=Permission, Post=Post, Comment=Comment)
-# manager.add_command("shell", Shell(make_context=make_shell_context))
-# manager.add_command('db', MigrateCommand)
-
-
-#
-# @manager.command
-# def test():
-#     """Run the unit tests."""
-#     import unittest
-#     tests = unittest.TestLoader().discover('tests')
-#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == '__main__':
-    # manager.run()
     http_server = WSGIServer(('', 5000), app)
     http_server.serve_forever()
