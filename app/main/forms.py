45264def58225e698fd138536e31bb69c35902ab
yukun <15152347277@163.com>
2017-07-28 09:58:46 +0800

    删除last分支

--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class SearchForm(FlaskForm):
-    name = StringField('你要搜索的小说名是？', validators=[DataRequired()])
-    submit = SubmitField('搜索')
-
