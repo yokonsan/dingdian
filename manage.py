e6b0919d86e7b62847fce1e0fde770adb62c2b61
yukun <15152347277@163.com>
2017-07-31 21:25:25 +0800

    注释

--- manage.py
+++ manage.py
@@ -14,21 +14,22 @@ manager.add_command('db', MigrateCommand)
 @manager.command
 def deploy():
     from flask_migrate import upgrade
-    from dingdian.models import Search, Novel, Chapter, Article
-
-    # 情况数据库
-    searchs = Search.query.all()
-    for s in searchs:
-        db.session.delete(s)
-    novels = Novel.query.all()
-    for n in novels:
-        db.session.delete(n)
-    chapters = Chapter.query.all()
-    for c in chapters:
-        db.session.delete(c)
-    articles = Article.query.all()
-    for a in articles:
-        db.session.delete(a)
+    # 情况数据库的操作只在运行过后才可以取消注释使用
+    # from dingdian.models import Search, Novel, Chapter, Article
+    #
+    # # 清空数据库
+    # searchs = Search.query.all()
+    # for s in searchs:
+    #     db.session.delete(s)
+    # novels = Novel.query.all()
+    # for n in novels:
+    #     db.session.delete(n)
+    # chapters = Chapter.query.all()
+    # for c in chapters:
+    #     db.session.delete(c)
+    # articles = Article.query.all()
+    # for a in articles:
+    #     db.session.delete(a)
 
     db.session.commit()
 
